a7c787c53c46d594d6ce4a7c33c38593a5e40053
SirVegetable <17wgs@queensu.ca>
2024-02-20 14:25:31 -0500

    added threading sol for listening and multiple functions

--- multiThreadTCP/tcpserver.cpp
+++ multiThreadTCP/tcpserver.cpp
@@ -1,13 +1,42 @@
 #include "tcpserver.hpp"
+#include<pthread.h>
 
 
 TCPserver::TCPserver()
 {
-
+    isListening = false; 
+    listeningPort = -1; 
 }
 
 TCPserver::~TCPserver()
 {
+
+}
+
+void TCPserver::Listen(int port, std::function<void(const std::string&)> callBack){
+
+    if(isListening) return;
+    this->listeningPort = port;
+    this->callBack = callBack; 
+    
+    // start thread
+    if(pthread_create(&listenerThread,nullptr,StartListeningProc(),this)!= 0){
+        throw "listening thread: failed to create"; 
+    }
+}
+void* StartListeningProc(void* param){
+    TCPserver* server = static_cast<TCPserver*>(param); 
+    try{
+        server->ListenThreadProc(); 
+    }
+    catch(...){
+        std::cerr << "Listening thread error" << std::endl; 
+    }
+    return nullptr; 
+}
+
+void TCPserver::Stop(){
+
 
 }
 
--- multiThreadTCP/tcpserver.hpp
+++ multiThreadTCP/tcpserver.hpp
@@ -1,15 +1,25 @@
 #ifndef TCP_SERVER_HPP
 #define TCP_SERVER_HPP
-#include <iostream>
-#include <vector> 
 #include "connections.hpp"
+#include <iostream>
+#include <vector>
+#include <functional> 
+#include <pthread.h>
 
 class TCPserver{
     public: 
         TCPserver(); 
-        ~TCPserver(); 
+        ~TCPserver();
 
-    private: 
+        void Listen(int port,std::function<void(const std::string&)> callBack);
+        void ListenThreadProc(int port, std::function<void(const std::string&)> callback);  
+        void Stop();  
+
+    private:
+        int listeningPort;
+        bool isListening;
+        pthread_t listenerThread; 
+        std::function<void(const std::string&)> callBack; 
         std::vector<Connection*> clientQueue; 
 
 }; 

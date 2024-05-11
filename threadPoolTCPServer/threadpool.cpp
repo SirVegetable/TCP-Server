d385216c277eadea034385952d6941efd9247a02
SirVegetable <17wgs@queensu.ca>
2024-05-11 17:33:49 -0400

    added implementation for destrcutor and constructor and fixed a couple of errors

--- threadPoolTCPServer/threadpool.cpp
+++ threadPoolTCPServer/threadpool.cpp
@@ -1,4 +1,36 @@
-#include<thread>
-#include<iostream> 
+#include <thread>
+#include <iostream>
+#include <chrono>
+#include "threadpool.hpp"
 
+
+ThreadPool::ThreadPool(std::size_t thread_count)
+{   
+    if(!thread_count)
+    {
+        throw std::invalid_argument("thread count error: must be non-zero"); 
+    }
+    for(auto i = 0; i < thread_count; ++i)
+    {
+        _pool.push_back(std::thread([this]()
+        {
+            std::cout << "thread: " << std::this_thread::get_id() << " begins... "<< std::endl; 
+            using namespace std::chrono_literals; 
+            std::this_thread::sleep_for(1s); 
+        })); 
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    for(auto& t: _pool)
+    {
+        t.join(); 
+    }
+}
+
+void ThreadPool::start_work(work_item w_item)
+{
+
+}
 
--- threadPoolTCPServer/threadpool.hpp
+++ threadPoolTCPServer/threadpool.hpp
@@ -2,30 +2,31 @@
 #include <queue>
 #include <mutex> 
 
+#pragma once
 
 class ThreadPool{
     public:
-        ThreadPool(std::size_t thread_count = std::thread::hardware_concurrency()){}
+        ThreadPool(std::size_t thread_count = std::thread::hardware_concurrency()); 
         ThreadPool(const ThreadPool&) = delete;
         ThreadPool(ThreadPool&&) = delete;
         ThreadPool& operator=(const ThreadPool&) = delete; 
         ThreadPool& operator=(ThreadPool&& )= delete; 
-        ~ThreadPool(){}
+        ~ThreadPool(); 
 
     public: 
         using work_item = std::function<void(void)> ;
 
-        void start_work(work_item wItem); 
+        void start_work(work_item w_item); 
         
     private:
         using work_item_ptr = std::unique_ptr<work_item>; 
         using work_queue = std::queue<work_item_ptr>; 
-
+        using Pool = std::vector<std::thread>; 
         work_queue _queue;
         std::mutex _queue_lock; 
         std::condition_variable _condition; 
-        bool _check; 
-
+        bool _check = false; 
+        Pool _pool; 
 
 
 }; 

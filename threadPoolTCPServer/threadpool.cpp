8296937a8947396a571a5296e5c55ed6ed37831a
SirVegetable <17wgs@queensu.ca>
2024-07-15 22:42:14 -0400

    sentinels

--- threadPoolTCPServer/threadpool.cpp
+++ threadPoolTCPServer/threadpool.cpp
@@ -1,7 +1,10 @@
 #include <thread>
 #include <iostream>
 #include <chrono>
+#include <utility> 
+#include <memory> 
 #include "threadpool.hpp"
+
 
 
 ThreadPool::ThreadPool(std::size_t thread_count)
@@ -23,6 +26,11 @@ ThreadPool::ThreadPool(std::size_t thread_count)
 
 ThreadPool::~ThreadPool()
 {
+    {
+        std::unique_lock guard(_queue_lock); 
+        _queue.push(work_item_ptr{nullptr});
+        _condition.notify_one(); 
+    }
     for(auto& t: _pool)
     {
         t.join(); 
@@ -31,6 +39,13 @@ ThreadPool::~ThreadPool()
 
 void ThreadPool::start_work(work_item w_item)
 {
+    auto item = std::make_unique<work_item>(std::move(w_item)); 
+    {
+        std::unique_lock guard(_queue_lock); 
+        _queue.push(std::move(item));
+        _check = true;
+    }
+    _condition.notify_one(); 
 
 }
 
